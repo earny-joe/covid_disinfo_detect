c8bc88e6478a77a014a7e75b00d1ad3819de0d5b
Joseph Earnshaw <jearnshaw58@gmail.com>
2020-03-27 00:17:17 +0000

    update March 26: notebook 1.0 and 2.0 detailing intial steps taken to clean & explore data

---
+++ experiments/fundamentals.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+
+def load_data(origpath, datapath, filename):
+    """
+    Given path to a specific data directory, loads in data from given filename
+    """
+    # change directory to where data is located
+    os.chdir(datapath)
+    # load in data with given filename
+    df = pd.read_pickle(filename)
+    # change directory back to original
+    os.chdir(origpath)
+    # return dataframe
+    return df
