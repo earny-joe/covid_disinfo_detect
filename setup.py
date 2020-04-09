133e9cf341e241752e9245cd4b3eb7a56abf026d
Joseph Earnshaw <jearnshaw58@gmail.com>
2020-04-09 18:29:39 +0000

    adding updates to setup.py file

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import find_packages, setup
 
+def list_reqs(fname='requirements.txt'):
+    with open(fname) as f:
+        return f.read().splitlines()
+
 setup(
     name='src',
     packages=find_packages(),
@@ -7,4 +11,5 @@ setup(
     description='A project the attempts to detect instances of disinformation related to COVID-19 on Twitter.',
     author='Joseph Earnshaw',
     license='MIT',
+    install_requires=list_reqs()
 )
