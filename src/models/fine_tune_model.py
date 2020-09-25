edfbcae22873b6581cd85249f697ebeb7ec10dc6
Joseph Earnshaw <jearnshaw58@gmail.com>
2020-09-25 22:35:58 +0000

    updates: made changes to requirements.txt & fine_tune_model.py files

--- src/models/fine_tune_model.py
+++ src/models/fine_tune_model.py
@@ -1,13 +1,13 @@
 """
 Beginning work towards script that'll fine-tune model for tweets
 """
-import spacy_sentence_bert
-from sentence_transformers import SentencesDataset, InputExample
+from transformers import AutoModelForMaskedLM, AutoTokenizer
 import utils
 import pandas as pd
 import settings.config as config
 from datetime import datetime
 import numpy as np
+import random
 
 
 def load_daily_data(bucket_path):
@@ -34,12 +34,12 @@ def load_data_wrapper(parquet_files):
     necessary text processing, and return a dataframe that includes
     data from all available daily Twitter data.
     """
-    # testing on five files
-    # test = parquet_files[:5]
+    # testing on two files
+    test = parquet_files[:2]
 
     begin = datetime.now()
     df = pd.concat([
-        load_daily_data(f'gs://{config.BUCKET_NAME}/{file}') for file in parquet_files
+        load_daily_data(f'gs://{config.BUCKET_NAME}/{file}') for file in test
     ]).reset_index(drop=True)
 
     df = utils.clean_for_embeddings(df)
@@ -48,21 +48,19 @@ def load_data_wrapper(parquet_files):
     print(f'\nLoading in {len(parquet_files)} files took {end - begin}.\n')
 
     return df
-    
-    
 
-def gather_training_examples(df, model):
-    """
-    Gather training examples for fine-tuning model.
-    """
-    # gather all tweets
-    tweets = df['normalized_tweet'].tolist()
-    # pass into train_examples object
-    # train_samples = []
-    train_examples = InputExample(texts=tweets)
-    train_dataset = SentencesDataset(train_examples, model)
 
-    return train_dataset
+def load_transformer_model():
+    """
+    TBD
+    """
+    tokenizer = AutoTokenizer.from_pretrained(
+        f'sentence-transformers/{config.EMBED_MODEL_NAME}'
+    )
+    model = AutoModelForMaskedLM.from_pretrained(
+        f'sentence-transformers/{config.EMBED_MODEL_NAME}'
+    )
+    return tokenizer, model
 
 
 def main():
@@ -72,14 +70,27 @@ def main():
     # set seeds
     utils.set_seed()
     # load transformers model
-    model = utils.create_embedding_model()
-    #print(model.max_seq_length)
-    
-    # gather list of parquet files containing data
-    #parquet_files = utils.list_parquet_tweet_files()[::-1]
+    tokenizer, model = load_transformer_model()
 
-    #df = load_data_wrapper(parquet_files)
-    #norm_tweets = num_tokens_avg(df)
+    print(model, '\n\n')
+
+    # gather list of parquet files containing data
+    parquet_files = utils.list_parquet_tweet_files()[::-1]
+
+    df = load_data_wrapper(parquet_files)
+    print(df.info(), '\n')
+
+    all_texts = np.array(df['normalized_tweet'].values)
+    print(all_texts.shape, '\n')
+
+    num = random.randint(0, len(df))
+    test_tweet = all_texts[num]
+    print(test_tweet, '\n')
+    test_ids = tokenizer.encode(test_tweet)
+    print(test_ids, '\n')
+    print(tokenizer.decode(test_ids))
+
+    # norm_tweets = num_tokens_avg(df)
     # print(norm_tweets.mean(), norm_tweets.std(), norm_tweets.min(), norm_tweets.max())
 
 
